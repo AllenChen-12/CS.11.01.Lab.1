643a22b22f6bdb8a4c2ffc2b4bef462666bd6a4b
AllenChen-12 <allenc2028@student.cis.edu.hk>
2025-09-18 08:52:56 +0800

    pages

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        int pages = 100 - age;
+        System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
+        scanner.close();
     }
 }
